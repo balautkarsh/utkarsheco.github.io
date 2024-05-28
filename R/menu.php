3f4ab46209d315b0cd74d61bbded97f7d9517bd9
balautkarsh <balautkarsh@gmail.com>
2024-05-29 01:01:58 +0530

    update

---
+++ R/menu.php
@@ -0,0 +1,9 @@
+<? php
+<ul>
+							<li><a href="intro.html">Introduction</a></li>
+							<li><a href="generic.html">Ipsum veroeros</a></li>
+							<li><a href="generic.html">Tempus etiam</a></li>
+							<li><a href="generic.html">Consequat dolor</a></li>
+							<li><a href="elements.html">Elements</a></li>
+</ul>
+?>
